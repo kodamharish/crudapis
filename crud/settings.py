f187c61fbd459727c902e2c9ccc6566ff40292af
srisudhatri <113518285+srisudhatri@users.noreply.github.com>
2024-06-02 19:14:07 +0530

    updated

--- crud/settings.py
+++ crud/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Location where static files will be collected
+
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
